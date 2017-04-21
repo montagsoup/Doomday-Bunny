373a600a0bf68f5a04f4291415ad4ea2d13ef38f
Maxakipust <Maxakipust@gmail.com>
2017-04-21 08:59:33 -0600

    Added Background

---
+++ Dooms Day Bunny/Assets/Scripts/Background_Move.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background_Move : MonoBehaviour {
+    public GameObject Bground0;
+    public GameObject Bground1;
+    public GameObject Bground2;
+    public Camera Cam;
+
+    private 
+
+    // Use this for initialization
+    void Start () {
+        Bground0Renderer = Bground0.GetComponent<SpriteRenderer>().bounds.;
+        Bground1
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
