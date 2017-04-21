876d4788c61cbcaa95616abdbc2cc2e15b3a7c3c
Maxakipust <Maxakipust@gmail.com>
2017-04-21 09:40:30 -0600

    The background moves

--- Dooms Day Bunny/Assets/Scripts/Background_Move.cs
+++ Dooms Day Bunny/Assets/Scripts/Background_Move.cs
@@ -5,19 +5,30 @@ using UnityEngine;
 public class Background_Move : MonoBehaviour {
     public GameObject Bground0;
     public GameObject Bground1;
-    public GameObject Bground2;
-    public Camera Cam;
 
-    private 
+    private float speed = 0.1f;
+    //public Camera Cam;
+
+    private Bounds BgroundBounds;
 
     // Use this for initialization
     void Start () {
-        Bground0Renderer = Bground0.GetComponent<SpriteRenderer>().bounds.;
-        Bground1
+        BgroundBounds = Bground0.GetComponent<SpriteRenderer>().bounds;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        Bground0.transform.position = new Vector3(Bground0.transform.position.x - speed, 0);
+        Bground1.transform.position = new Vector3(Bground1.transform.position.x - speed, 0);
+
+        if(Bground0.transform.position.x < -BgroundBounds.size.x)
+        {
+            Bground0.transform.position = new Vector3(Bground0.transform.position.x + BgroundBounds.size.x * 2,0); 
+        }
+        if (Bground1.transform.position.x < -BgroundBounds.size.x)
+        {
+            Bground1.transform.position = new Vector3(Bground1.transform.position.x + BgroundBounds.size.x * 2, 0);
+        }
+        speed += speed / 10000;
+    }
 }
